237f38d718367e6df2e22d77b4aa0ec400723789
Abir Hasan Dipu <dipu.softifybd@gmail.com>
2021-01-08 23:09:29 +0600

    Basic digital clock

---
+++ clock_1/index.js
@@ -0,0 +1,31 @@
+function showTime(){
+    var date = new Date();
+
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    var ampm = "AM";
+
+    if(h==12){
+        h = 12;
+    }
+    if(h>12){
+        h = h - 12;
+        ampm = "PM";
+    }
+
+    h = (h<10) ? "0" + h : h;
+    m = (m<10) ? "0" + m : m;
+    s = (s<10) ? "0" + s : s;
+
+    var time = h + " : " + m + " : " + s + " " + ampm;
+
+    document.getElementById("clockText").innerText = time;
+
+    document.getElementById("clockText").textContent = time;
+
+    setTimeout(showTime, 1000);
+}
+
+showTime();
+
